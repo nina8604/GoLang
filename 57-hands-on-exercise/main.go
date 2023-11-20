70517bb2f08d53ae09e6ef126afade375712a8c8
nina8604 <nina.yaremenko86@gmail.com>
2023-11-20 17:23:57 +0200

    57 hands on exercise

---
+++ 57-hands-on-exercise/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nina8604/puppy"
+)
+
+func main() {
+	fmt.Println("Hello Gopher")
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(s4)
+	puppy.From13()
+}
