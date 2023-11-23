2043c69aeb4f34a04bea8936e442d0560b3800e0
nina8604 <nina.yaremenko86@gmail.com>
2023-11-23 09:57:11 +0200

    add 80 hands on exercise

---
+++ 80-hands-on-exercise/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	for i := 0; i < 42; i++ {
+		x := rand.Intn(5)
+		fmt.Printf("iteration %v\t", i)
+
+		switch x {
+		case 0:
+			fmt.Println("x is 0")
+		case 1:
+			fmt.Println("x is 1")
+		case 2:
+			fmt.Println("x is 2")
+		case 3:
+			fmt.Println("x is 3")
+		case 4:
+			fmt.Println("x is 4")
+		default:
+			fmt.Println("this is default case for x")
+		}
+	}
+}
