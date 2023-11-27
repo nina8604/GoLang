89f532bcb9c01c5d291835b642d755c180125f60
nina8604 <nina.yaremenko86@gmail.com>
2023-11-27 10:48:24 +0200

    multidemensional slice

---
+++ 102-multidemensional-slice/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	jb := []string{"James", "Bond", "Martini", "Chocolate"}
+	jm := []string{"Janny", "Moneypenny", "Guiness", "Wolvarine Tracks"}
+	fmt.Println(jb)
+	fmt.Println(jm)
+
+	xxs := [][]string{jb, jm}
+	fmt.Println(xxs)
+
+}
