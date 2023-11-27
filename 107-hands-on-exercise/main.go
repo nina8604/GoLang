53547603a4b3b6bd5174eeae970a015b946f2493
nina8604 <nina.yaremenko86@gmail.com>
2023-11-27 12:30:52 +0200

    107 hands on exercise

---
+++ 107-hands-on-exercise/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		x[i] = 42 + i
+	}
+
+	for i, v := range x {
+		fmt.Printf("%v - %T - %v\n", v, v, i)
+	}
+
+	fmt.Printf("%T \t %#v\n", x, x)
+	fmt.Printf("%T \t %v\n", x, x)
+}
