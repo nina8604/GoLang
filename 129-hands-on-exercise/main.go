0a8706859fb943633b764424a1593dde2b87461f
nina8604 <nina.yaremenko86@gmail.com>
2023-11-30 10:40:57 +0200

    129 exercise

---
+++ 129-hands-on-exercise/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	an := struct {
+		first     string
+		friends   map[string]int
+		favDrinks []string
+	}{
+		first: "Nina",
+		friends: map[string]int{
+			"Inna":   39,
+			"Nastia": 38,
+		},
+		favDrinks: []string{"Martini", "Beer"},
+	}
+
+	fmt.Println(an)
+	fmt.Printf("%#v", an)
+
+}
