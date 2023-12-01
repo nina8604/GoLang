b6741b0efa9cddad648ebd8ca648d84635f91ca4
nina8604 <nina.yaremenko86@gmail.com>
2023-12-01 17:49:10 +0200

    152 exercise

---
+++ 153-hands-on-exercise/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer foo()
+	defer bar()
+	karaoke()
+}
+
+func foo() {
+	fmt.Println("Hello from foo")
+}
+func bar() {
+	fmt.Println("Hello from bar")
+}
+func karaoke() {
+	fmt.Println("Hello from karaoke")
+}
