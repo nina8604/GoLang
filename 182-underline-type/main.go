55bb47c7a773247cf62b94eea6cb32c1e6115eed
nina8604 <nina.yaremenko86@gmail.com>
2023-12-06 16:21:15 +0200

    underline type

--- 182-underline-type/main.go
+++ 182-underline-type/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
 
 func addI(a, b int) int {
 	return a + b
@@ -10,22 +14,20 @@ func addF(a, b float64) float64 {
 	return a + b
 }
 
-type myNumbers interface {
-	~int | ~float64
+type myNums interface {
+	constraints.Integer | constraints.Float
 }
 
-func addT[T myNumbers](a, b T) T {
+func addT[T myNums](a, b T) T {
 	return a + b
 }
 
-type myAlias int
+type meAlias int
 
 func main() {
-	var n myAlias = 37
+	var n meAlias = 12
 	fmt.Println(addI(1, 2))
 	fmt.Println(addF(1.2, 2.2))
-
 	fmt.Println(addT(n, 2))
 	fmt.Println(addT(1.2, 2.2))
-
 }
