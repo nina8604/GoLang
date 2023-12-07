74b18bd24010c1dd50d49a611dea74033d0c82f0
nina8604 <nina.yaremenko86@gmail.com>
2023-12-07 12:06:27 +0200

    custom sort

---
+++ 186-sort-custom/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+type ByAge []Person
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+type ByName []Person
+
+func (bn ByName) Len() int           { return len(bn) }
+func (bn ByName) Swap(i, j int)      { bn[i], bn[j] = bn[j], bn[i] }
+func (bn ByName) Less(i, j int) bool { return bn[i].Name < bn[j].Name }
+
+func main() {
+	people := []Person{
+		{"Bob", 31},
+		{"John", 42},
+		{"Michael", 17},
+		{"Jenny", 26},
+	}
+
+	fmt.Println(people)
+	// sort by Age
+	sort.Sort(ByAge(people))
+	fmt.Println(people)
+
+	// sort by Name
+	sort.Sort(ByName(people))
+	fmt.Println(people)
+
+}
