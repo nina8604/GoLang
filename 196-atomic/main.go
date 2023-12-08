35d37219adaeb2b7bc4c0a9245a2aa5e287f8216
nina8604 <nina.yaremenko86@gmail.com>
2023-12-08 16:56:26 +0200

    exercise with goroutine

--- 196-atomic/main.go
+++ 196-atomic/main.go
@@ -4,31 +4,31 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
-	"sync/atomic"
 )
 
 func main() {
-	fmt.Println("CPUs:", runtime.NumCPU())
-	fmt.Println("Goroutines:", runtime.NumGoroutine())
-	var counter int64
-	const gs = 100
 
+	fmt.Println("begin CPUs:", runtime.NumCPU())
+	fmt.Println("begin Goroutines:", runtime.NumGoroutine())
 	var wg sync.WaitGroup
-	wg.Add(gs)
-	for i := 0; i < gs; i++ {
-		go func() {
-			atomic.AddInt64(&counter, 1)
-			runtime.Gosched()
+	wg.Add(2)
+	go func() {
+		fmt.Println("Hello from first func")
+		wg.Done()
 
-			fmt.Println("Counter:", atomic.LoadInt64(&counter))
+	}()
 
-			wg.Done()
-		}()
-		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	go func() {
+		fmt.Println("Hello from second func")
+		wg.Done()
+	}()
 
-	}
+	fmt.Println("mid CPUs:", runtime.NumCPU())
+	fmt.Println("mid Goroutines:", runtime.NumGoroutine())
+
 	wg.Wait()
-	fmt.Println("Goroutines:", runtime.NumGoroutine())
-	fmt.Println("count:", counter)
+	fmt.Println("exit of the program")
 
+	fmt.Println("end CPUs:", runtime.NumCPU())
+	fmt.Println("end Goroutines:", runtime.NumGoroutine())
 }
