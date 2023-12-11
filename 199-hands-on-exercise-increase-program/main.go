6ad5030d91eebd623761e3aabcd13e0542d234aa
nina8604 <nina.yaremenko86@gmail.com>
2023-12-11 10:46:18 +0200

    199 exercise with increase program

---
+++ 199-hands-on-exercise-increase-program/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	incrementer := 0
+	gs := 100
+
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			fmt.Println(incrementer)
+			v := incrementer
+			runtime.Gosched()
+			v++
+			incrementer = v
+			fmt.Println(incrementer)
+			wg.Done()
+		}()
+
+	}
+
+	wg.Wait()
+	fmt.Println("end value", incrementer)
+}
