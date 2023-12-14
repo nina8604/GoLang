cfcd78bd43bb85bfa1dae2e969ea3c8b297a4a14
nina8604 <nina.yaremenko86@gmail.com>
2023-12-14 12:28:43 +0200

    create dog package

---
+++ 226-hands-on-exercise-documentation/dog/main.go
@@ -0,0 +1,7 @@
+// Package dog provides solutions for counting dog years
+package dog
+
+// Years takes human years and turns them into dog years (1 human year = 7 dog years)
+func Years(ha int) int {
+	return int(ha / 7)
+}
