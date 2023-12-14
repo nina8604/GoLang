399236d0556f3386044edbda923d86906405415d
nina8604 <nina.yaremenko86@gmail.com>
2023-12-14 12:45:07 +0200

    change folder name

--- 226-hands-on-exercise-documentation/main.go
+++ 226-hands-on-exercise-documentation/main.go
@@ -2,31 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
+
+	"github.com/nina8604/GoLang/tree/master/226-hands-on-exercise-documentation/dog"
 )
 
-type sqrtError struct {
-	lat  string
-	long string
-	err  error
-}
-
-func (se sqrtError) Error() string {
-	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
-}
-
 func main() {
-	_, err := sqrt(-10.23)
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func sqrt(f float64) (float64, error) {
-	if f < 0 {
-		// e := errors.New("this is the sqrt err")
-		e := fmt.Errorf("this is the sqrt err - %v", f)
-		return 0, sqrtError{"50.2289 N", "99.4656 W", e}
-	}
-	return 42, nil
+	ha := 28
+	da := dog.Years(ha)
+	fmt.Printf("human - %v years turn to dog years - %v", ha, da)
 }
